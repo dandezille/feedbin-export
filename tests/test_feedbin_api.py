553f2bfc6f312535ece6022e2a5a854a33880136
Dan de Zille <dan@ddez.net>
2020-08-05 09:38:04 +0100

    Tests working with pytest

--- tests/test_feedbin_api.py
+++ tests/test_feedbin_api.py
@@ -1,25 +1,42 @@
+import base64
+
+import pytest
 import requests
 import responses
-import pdb
-from nose.tools import assert_equals
 
-import feedbin_api as feedbin
+import src.feedbin_api as feedbin
+
+
+@pytest.fixture(autouse=True)
+def run_around_tests(monkeypatch):
+    monkeypatch.setenv("FEEDBIN_USER", "user")
+    monkeypatch.setenv("FEEDBIN_PASSWORD", "password")
+    yield
+
+
+def has_auth_header(request):
+    return request.headers["Authorization"] == "Basic {}".format(
+        base64.b64encode(b"user:password").decode("ascii")
+    )
 
 
 def test_api_url():
-    assert_equals(feedbin._api_url("test.json"), "https://api.feedbin.com/v2/test.json")
+    assert feedbin._api_url("test.json") == "https://api.feedbin.com/v2/test.json"
 
 
 @responses.activate
-def test_check_authenticated_true():
+def test_check_authenticated_true(monkeypatch):
     responses.add(responses.GET, feedbin._api_url("authentication.json"), status=200)
-    assert_equals(feedbin.check_authenticated(), True)
-    assert_equals(len(responses.calls), 1)
-    assert_equals(responses.calls[0].request.headers, {})
+
+    assert feedbin.check_authenticated() == True
+    assert len(responses.calls) == 1
+    assert has_auth_header(responses.calls[0].request)
 
 
 @responses.activate
-def test_check_authenticated_false():
+def test_check_authenticated_false(monkeypatch):
     responses.add(responses.GET, feedbin._api_url("authentication.json"), status=401)
-    assert_equals(feedbin.check_authenticated(), False)
-    assert_equals(len(responses.calls), 1)
+
+    assert feedbin.check_authenticated() == False
+    assert len(responses.calls) == 1
+    assert has_auth_header(responses.calls[0].request)
