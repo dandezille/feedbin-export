fc01c6c797ede2b887d672a489a1706968cbd78d
Dan de Zille <dan@ddez.net>
2020-08-12 21:06:05 +0100

    Clearer import

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import os
 
 from dotenv import load_dotenv
 
-from .feedbin_to_todoist import feedbin_to_todoist
+from feedbin_to_todoist import export
+
 
 def main():
-    print("Loading environment")
-    load_dotenv()
-
-    feedbin_to_todoist(
+    export(
         os.getenv("FEEDBIN_USER"),
         os.getenv("FEEDBIN_PASSWORD"),
         os.getenv("TODOIST_API_KEY"),
     )
 
+
 if __name__ == "__main__":
+    print("Loading environment")
+    load_dotenv()
+
     main()
